a2ad041cd334fbb4e48061eea8691962ba5474e5
Эмиль <gabdulvaleev.emil@mail.ru>
2021-02-10 18:41:21 +0300

    Cosmetic update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit
 SCREEN_SIZE = [600, 450]
 
 
-class Example(QWidget):
+class MapWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.getImage()
@@ -18,17 +18,23 @@ class Example(QWidget):
         self.map_request = ''
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+        server = "http://static-maps.yandex.ru/1.x/"
+        parameters = {'ll': ','.join(map(str, self.coordinates)),
+                      'z': self.zoom,
+                      'l': self.map_type}
+        response = requests.get(server, params=parameters)
 
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
-
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
+
+        self.coordinates = [37.530887, 55.703118]
+        self.zoom = 1.0
+        self.map_type = 'map'
 
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
@@ -43,6 +49,6 @@ class Example(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = MapWindow()
     ex.show()
     sys.exit(app.exec())
